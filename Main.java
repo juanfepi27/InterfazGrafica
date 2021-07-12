70862bc2d0a04630284e5bf16ff5a0e6b69af086
juanfepi27 <85038450+juanfepi27@users.noreply.github.com>
2021-07-12 11:55:49 -0500

    12 07 11:55
    corrección de caracteres especiales

--- Main.java
+++ Main.java
@@ -9,29 +9,29 @@ public class Main {
     public static void main(String[] args) {
         
         VentanaJF ventana1 = new VentanaJF();
-        reporte("salió el frame");
+        reporte("sale el frame");
 
         /*
             LabelJF texto1 = new LabelJF();
             reporte("creamos el label con texto");
 
             ventana1.add(texto1);
-            reporte("se añade el label al frame");
+            reporte("se agrega el label al frame");
 
             LabelJF imagen = new LabelJF("img/logo1.jpg");
             reporte("creamos el label con imagen");
 
             ventana1.add(imagen);
-            reporte("se añade el label al frame");
+            reporte("se agrega el label al frame");
         */
 
         LabelJF imagenYtexto = new LabelJF("img/logo1.jpg","combo de imagen y texto");
         reporte("creamos el label con imagen y texto");
 
         ventana1.add(imagenYtexto);
-        reporte("se añade el label al frame");
+        reporte("se agrega el label al frame");
 
-        ventana1.setLayout(null);//elimina la organización por default que se le dan a los elmentos en el frame y evita el autofit de los elementos como el label
+        ventana1.setLayout(null);//elimina la organizacion por default que se le dan a los elmentos en el frame y evita el autofit de los elementos como el label
 
         ventana1.setVisible(true);
 
